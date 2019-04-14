9e694e296c99ed925ab994a6634c2c581b0f9ccf
Yakubenko <alexander.yakubenko@gmail.com>
2019-04-14 18:48:13 -0500

    second commit

--- Test1.py
+++ Test1.py
@@ -1,2 +1,42 @@
+import math
+
 #Types
-int a = 3;
+print 'start'
+print (type (True))
+print (0b10)
+print (0x10)
+print (''' This is my sample string with quotes 'single' and "double" ''')
+
+print (max(14,13.9))
+#help (math)
+print bin (10)
+a=101
+print(bool(0))
+
+print (chr(97))
+print (complex(10,20))
+print (int ("102"))
+print (str(10+2))
+
+a = 10
+a1 = 10.0
+b = 20.2
+c = "ok"
+d = False
+my_objects = []
+
+my_objects.append(a)
+my_objects.append(a1)
+my_objects.append(b)
+my_objects.append(c)
+my_objects.append(d)
+
+for obj in my_objects:
+    print type(obj)
+
+s1="abcsefg"
+s2="a2d3f4v3"
+
+print all(my_objects)
+
+
